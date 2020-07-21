2d84db30ccb36cdca976ba1d07e55ae13e582f5d
Lynn <lynn@lynntheory.com>
2020-07-21 13:00:22 -0400

    copying tutorials again

--- cmds/role.js
+++ cmds/role.js
@@ -1,12 +1,11 @@
 const config = require('../config/config.json');
 
-
 async function role (message) {
-  var subject = message.member;
   try{
       if (message.content.match(/alerts/i)) {
-
-        subject.roles.add('733445445262508142');
+        let myRole = message.guild.roles.get("733445445262508142");
+        let subject = message.member;
+        member.roles.add(myRole);
         message.reply('Alerts role added.');
     } else {
       console.log('This role does not exist!');
