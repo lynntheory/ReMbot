394665a45acae5b5a0d721370f3a6a17d62400f6
Lynn <lynn@lynntheory.com>
2020-07-21 12:47:47 -0400

    fsk it, hard coding role number

--- cmds/role.js
+++ cmds/role.js
@@ -3,7 +3,7 @@ const config = require('../config/config.json');
 async function role (message) {
   try{
       if (message.content.match(/alerts/i)) {
-        var role = message.channel.guild.roles.cache.find(role => role.name === 'alerts');
+        var role = message.guild.roles.fetch('733445445262508142');
         message.member.roles.add(role);
         message.reply('Alerts role added.');
     } else {
