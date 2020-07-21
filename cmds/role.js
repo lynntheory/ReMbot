6a7afaa1845e960bdb694dec09b22cdf318038fa
Lynn <lynn@lynntheory.com>
2020-07-21 12:54:20 -0400

    changing variables on the subject of the message

--- cmds/role.js
+++ cmds/role.js
@@ -1,9 +1,12 @@
 const config = require('../config/config.json');
 
+
 async function role (message) {
+  var subject = message.member;
   try{
       if (message.content.match(/alerts/i)) {
-        message.member.roles.add('733445445262508142');
+
+        subject.roles.add('733445445262508142');
         message.reply('Alerts role added.');
     } else {
       console.log('This role does not exist!');
