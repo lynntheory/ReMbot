92981bab55487fb481c7a36060a6e93477c17abc
Lynn <lynn@lynntheory.com>
2020-07-21 12:49:59 -0400

    even less flex

--- cmds/role.js
+++ cmds/role.js
@@ -3,8 +3,7 @@ const config = require('../config/config.json');
 async function role (message) {
   try{
       if (message.content.match(/alerts/i)) {
-        var role = message.guild.roles.fetch('733445445262508142');
-        message.member.roles.add(role);
+        message.member.roles.add('733445445262508142');
         message.reply('Alerts role added.');
     } else {
       console.log('This role does not exist!');
