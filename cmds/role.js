191e5861d79a2ff2daf4c9161abe40bdc0d38e5b
Lynn <lynn@lynntheory.com>
2020-07-21 12:40:57 -0400

    is it 'read only' issue?

--- cmds/role.js
+++ cmds/role.js
@@ -3,7 +3,7 @@ const config = require('../config/config.json');
 async function role (message) {
   try{
       if (message.content.match(/alerts/i)) {
-        var role = message.guild.roles.cache.find(role => role.name === 'alerts');
+        var role = message.channel.guild.roles.cache.find(role => role.name === 'alerts');
         message.member.roles.add(role);
         message.reply('Alerts role added.');
     } else {
